65ca2a1ff440c93ce3304b5625299e8d6c8c3163
Andrea F. Daniele <afdaniele@ttic.edu>
2019-12-04 12:59:43 -0600

    added /environment.sh to launchfile

--- launch.sh
+++ launch.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-set -e
+source /environment.sh
+
+# initialize launch file
+dt_launchfile_init
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-echo "This is an empty launch script. Update it to launch your application."
+
+
+# NOTE: Use the variable CODE_DIR to know the absolute path to your code
+# NOTE: Use `dt_exec COMMAND` to run the main process (blocking process)
+
+# launching app
+dt_exec echo "This is an empty launch script. Update it to launch your application."
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# terminate launch file
+dt_launchfile_terminate
